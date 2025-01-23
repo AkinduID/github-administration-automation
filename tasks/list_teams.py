681225331fac7dea5d382925d9873363cc49c3a7
Akindu Delgahagoda <akindu@akindu.local>
2025-01-23 11:36:57 +0530

    created new function to list teams

---
+++ tasks/list_teams.py
@@ -0,0 +1,13 @@
+import requests
+
+def list_teams(organization, token):
+    url = f"https://api.github.com/orgs/{organization}/teams?per_page=100"
+    headers = {
+        "Authorization": f"token {token}"
+    }
+
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise Exception(f"Failed to fetch teams: {response.status_code} - {response.text}")

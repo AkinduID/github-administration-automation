d471d5587601aba9e3aca7456cc5eb8678fbb46f
Akindu Delgahagoda <akindu@akindu.local>
2025-01-27 10:19:36 +0530

    added comments explaining workflow with pseudo code

--- functions.py
+++ functions.py
@@ -17,10 +17,31 @@ __all__ = ["list_teams","get_pat", "create_repo", "add_topics","add_labels","add
 
 # 1. List all teams in the organization (Run Once) (can be run after step 8)
 # 2. Get PAT for org
-# 3. Create a new repository in the organization
+# 3. Create a new repository in the organization 
 # 4. Add Topics (Optional)
 # 5. Add Labels
-# 6. Add Issue Template if Issues are enabled
+# 6. Add Issue Template
 # 7. Add PR Template
 # 8. Add Branch Protection Rules
 # 9. Set Infra Team Permissions
+
+
+# Approve Request Flow
+# We need to add the following steps to the approve_request function in backend.py
+# pseduo code
+# def approve_request(repo_name: str):
+#     requests_list = read_requests()
+#     for request in requests_list:
+#         if request["repo_name"] == repo_name:
+#             if request["approval_state"] != "Pending":
+#                 raise HTTPException(status_code=400, detail="Request is already processed")
+#             request["approval_state"] = "Approved"
+#             write_requests(requests_list)
+#             create_repo()
+#             if requests["topics"]:
+#                   add_topics()
+#             add_labels()
+#             add_issue_template()
+#             add_pr_template()
+#             add_branch_protection()
+#             set_infra_team_permissions()

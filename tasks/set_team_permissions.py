1b15c3ce984269ca632c63703d5dc91b10738cfe
Akindu Delgahagoda <akindu@akindu.local>
2025-01-28 15:02:33 +0530

    renamed file

---
+++ tasks/set_team_permissions.py
@@ -0,0 +1,10 @@
+import requests
+
+# Function Description: Set the permissions for the infra team
+
+# ToDo
+# read the curl comands and create python code
+# add the functinaly of adding main teams
+
+def set_team_permissions(organization, team_slug, token):
+    pass

adf5bdc2d8e26681eaa0b6799f986ef1cb5430a3
Akindu Delgahagoda <akindu@akindu.local>
2025-01-28 10:39:07 +0530

    created get pat function

--- tasks/get_pat.py
+++ tasks/get_pat.py
@@ -1,4 +1,25 @@
-pat_dict = {"Org1","Pat1"}
+import json
 
-def get_pat(organization):
-    return pat_dict[organization]
+def get_pat(org):
+    try:
+        with open('pat_list.json', 'r') as file:
+            pat_list = json.load(file)
+        for pat_data in pat_list:
+            if org in pat_data:
+                return pat_data[org]
+        return None
+
+    except FileNotFoundError:
+        print("The file 'pat_list.json' was not found.")
+        return None
+    except json.JSONDecodeError:
+        print("Error decoding the JSON file.")
+        return None
+    
+# if __name__ == "__main__":
+#     organization = "GitOpsLab-3"
+#     pat = get_pat(organization)
+#     if pat:
+#         print(f"PAT for {organization}: {pat}")
+#     else:
+#         print(f"PAT for {organization} not found.")

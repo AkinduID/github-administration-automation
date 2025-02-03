ec064e22c558a83e66057087dfececff74436a0a
Akindu Delgahagoda <akindu@akindu.local>
2025-02-03 10:51:50 +0530

    called list teams function at app startup

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
-# app/main.py
 from fastapi import FastAPI
 from app.api.repo import router as repo_router
+from app.tasks.github_operations import list_teams  # Import the function
 
 app = FastAPI()
+
+@app.on_event("startup")
+async def startup_event():
+    print("Running list_teams at startup...")
+    list_teams()
 
 app.include_router(repo_router)

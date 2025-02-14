100b7d5d9b3db678d78d343874a45e9088736194
Akindu Delgahagoda <akindu@akindu.local>
2025-02-14 13:29:51 +0530

    removed extra comments

--- frontend/app.py
+++ frontend/app.py
@@ -1,10 +1,12 @@
 import streamlit as st
 
+# Page Description: Entry Point for frontend
+
+# ToDo 
+# intregrate asgardeo authentication
+
 st.title("Welcome to GitHub Repo Requests")
-st.write("Please select your role:")  # More descriptive message
+st.write("Please select your role:") 
 
 st.page_link("pages/user_dashboard.py", label="User", icon="1️⃣")
 st.page_link("pages/admin_dashboard.py", label="Admin", icon="2️⃣")
-
-# Optional: Add a message or instructions below the buttons
-# st.write("Click 'User' to access the user dashboard or 'Admin' to access the admin dashboard.")
